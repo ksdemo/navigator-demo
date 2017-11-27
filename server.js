79814d5410369d2119a4065111cab9bceef15d8e
Frank.Liang <frank.liang@yo-ren.com>
2017-11-27 15:06:57 +0800

    PORT changed

--- server.js
+++ server.js
@@ -7,4 +7,4 @@ app.use('/static', express.static(path.resolve(__dirname, 'dist', 'static')))
 app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'dist', 'index.html'))
 })
-app.listen(3000)
+app.listen(80)
